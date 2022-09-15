a2c2ad33ad8b637488b57d5597d4bf85e4063bc1
Arnaud293 <arnaudlt293@gmail.com>
2022-09-15 10:49:53 +0200

    SETTINGS: add variables, start of rendering

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const img = new Image();
 
+img.src='./media/flappy-bird-set.png';
+
+// Settings
+
+let gamePlaying = false;
+const gravity = .5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = (canvas.width / 10);
+
+let index = 0,
+    bestScore = 0,
+    currentScore = 0,
+    pipes = [],
+    flight,
+    flightHeight;
+
+const render = () => {
+    index ++ ;
+
+    ctx.drawImage()
+
+    window.requestAnimationFrame(render);
+}
+
+img.onload = render;
